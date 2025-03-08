884266c9d625faad96f45d20375a00269b176882
TEJAS PAWAR <98679722+Tejas2620@users.noreply.github.com>
2025-03-08 11:04:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,45 @@
-import gdown
-import os
+@ -0,0 +1,227 @@
 from flask import Flask, render_template, redirect, request, url_for, send_file
 from flask import jsonify, json
 from werkzeug.utils import secure_filename
+
+# Interaction with the OS
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+# Used for DL applications, computer vision related processes
 import torch
 import torchvision
+
+# For image preprocessing
 from torchvision import transforms
+
+# Combines dataset & sampler to provide iterable over the dataset
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
+
 import numpy as np
 import cv2
+
+# To recognise face from extracted frames
 import face_recognition
+
+# Autograd: PyTorch package for differentiation of all operations on Tensors
+# Variable are wrappers around Tensors that allow easy automatic differentiation
 from torch.autograd import Variable
+
 import time
+
 import sys
+
+# 'nn' Help us in creating & training of neural network
 from torch import nn
+
+# Contains definition for models for addressing different tasks i.e. image classification, object detection e.t.c.
 from torchvision import models
+
 from skimage import img_as_ubyte
 import warnings
-
 warnings.filterwarnings("ignore")
 
 UPLOAD_FOLDER = 'Uploaded_Files'
@@ -29,97 +50,121 @@ detectOutput = []
 app = Flask("__main__", template_folder="templates")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# URL of the model file on Google Drive
-model_url = 'https://drive.google.com/uc?id=1ya4E5gnw1Xf21Is-QX3dyHI21yJOu7Cx'
-model_path = 'model/df_model.pt'
-
-# Download the model file if it doesn't exist
-if not os.path.exists(model_path):
-    os.makedirs(os.path.dirname(model_path), exist_ok=True)
-    gdown.download(model_url, model_path, quiet=False)
-
 # Creating Model Architecture
-class Model(nn.Module):
-    def __init__(self, num_classes, latent_dim=2048, lstm_layers=1, hidden_dim=2048, bidirectional=False):
-        super(Model, self).__init__()
-        model = models.resnext50_32x4d(pretrained=True)
-        self.model = nn.Sequential(*list(model.children())[:-2])
-        self.lstm = nn.LSTM(latent_dim, hidden_dim, lstm_layers, bidirectional)
-        self.relu = nn.LeakyReLU()
-        self.dp = nn.Dropout(0.4)
-        self.linear1 = nn.Linear(2048, num_classes)
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
 
-    def forward(self, x):
-        batch_size, seq_length, c, h, w = x.shape
-        x = x.view(batch_size * seq_length, c, h, w)
-        fmap = self.model(x)
-        x = self.avgpool(fmap)
-        x = x.view(batch_size, seq_length, 2048)
-        x_lstm, _ = self.lstm(x, None)
-        return fmap, self.dp(self.linear1(x_lstm[:, -1, :]))
+class Model(nn.Module):
+  def __init__(self, num_classes, latent_dim= 2048, lstm_layers=1, hidden_dim=2048, bidirectional=False):
+    super(Model, self).__init__()
+
+    # returns a model pretrained on ImageNet dataset
+    model = models.resnext50_32x4d(pretrained= True)
+
+    # Sequential allows us to compose modules nn together
+    self.model = nn.Sequential(*list(model.children())[:-2])
+
+    # RNN to an input sequence
+    self.lstm = nn.LSTM(latent_dim, hidden_dim, lstm_layers, bidirectional)
+
+    # Activation function
+    self.relu = nn.LeakyReLU()
+
+    # Dropping out units (hidden & visible) from NN, to avoid overfitting
+    self.dp = nn.Dropout(0.4)
+
+    # A module that creates single layer feed forward network with n inputs and m outputs
+    self.linear1 = nn.Linear(2048, num_classes)
+
+    # Applies 2D average adaptive pooling over an input signal composed of several input planes
+    self.avgpool = nn.AdaptiveAvgPool2d(1)
+
+
+
+  def forward(self, x):
+    batch_size, seq_length, c, h, w = x.shape
+
+    # new view of array with same data
+    x = x.view(batch_size*seq_length, c, h, w)
+
+    fmap = self.model(x)
+    x = self.avgpool(fmap)
+    x = x.view(batch_size, seq_length, 2048)
+    x_lstm,_ = self.lstm(x, None)
+    return fmap, self.dp(self.linear1(x_lstm[:,-1,:]))
+
+
+
 
 im_size = 112
+
 mean = [0.485, 0.456, 0.406]
+
 std = [0.229, 0.224, 0.225]
+
 sm = nn.Softmax()
-inv_normalize = transforms.Normalize(mean=-1 * np.divide(mean, std), std=np.divide([1, 1, 1], std))
+
+inv_normalize = transforms.Normalize(mean=-1*np.divide(mean, std), std=np.divide([1,1,1], std))
 
 def im_convert(tensor):
-    image = tensor.to("cpu").clone().detach()
-    image = image.squeeze()
-    image = inv_normalize(image)
-    image = image.numpy()
-    image = image.transpose(1, 2, 0)
-    image = image.clip(0, 1)
-    cv2.imwrite('./2.png', image * 255)
-    return image
+  image = tensor.to("cpu").clone().detach()
+  image = image.squeeze()
+  image = inv_normalize(image)
+  image = image.numpy()
+  image = image.transpose(1,2,0)
+  image = image.clip(0,1)
+  cv2.imwrite('./2.png', image*255)
+  return image
 
 def predict(model, img, path='./'):
-    fmap, logits = model(img.to())
-    params = list(model.parameters())
-    weight_softmax = model.linear1.weight.detach().cpu().numpy()
-    logits = sm(logits)
-    _, prediction = torch.max(logits, 1)
-    confidence = logits[:, int(prediction.item())].item() * 100
-    print('confidence of prediction: ', logits[:, int(prediction.item())].item() * 100)
-    return [int(prediction.item()), confidence]
+  fmap, logits = model(img.to())
+  params = list(model.parameters())
+  weight_softmax = model.linear1.weight.detach().cpu().numpy()
+  logits = sm(logits)
+  _, prediction = torch.max(logits, 1)
+  confidence = logits[:, int(prediction.item())].item()*100
+  print('confidence of prediction: ', logits[:, int(prediction.item())].item()*100)
+  return [int(prediction.item()), confidence]
 
+
+# To validate the dataset
 class validation_dataset(Dataset):
-    def __init__(self, video_names, sequence_length=60, transform=None):
-        self.video_names = video_names
-        self.transform = transform
-        self.count = sequence_length
+  def __init__(self, video_names, sequence_length = 60, transform=None):
+    self.video_names = video_names
+    self.transform = transform
+    self.count = sequence_length
 
-    def __len__(self):
-        return len(self.video_names)
+  # To get number of videos
+  def __len__(self):
+    return len(self.video_names)
 
-    def __getitem__(self, idx):
-        video_path = self.video_names[idx]
-        frames = []
-        a = int(100 / self.count)
-        first_frame = np.random.randint(0, a)
-        for i, frame in enumerate(self.frame_extract(video_path)):
-            faces = face_recognition.face_locations(frame)
-            try:
-                top, right, bottom, left = faces[0]
-                frame = frame[top:bottom, left:right, :]
-            except:
-                pass
-            frames.append(self.transform(frame))
-            if len(frames) == self.count:
-                break
-        frames = torch.stack(frames)
-        frames = frames[:self.count]
-        return frames.unsqueeze(0)
+  # To get number of frames
+  def __getitem__(self, idx):
+    video_path = self.video_names[idx]
+    frames = []
+    a = int(100 / self.count)
+    first_frame = np.random.randint(0,a)
+    for i, frame in enumerate(self.frame_extract(video_path)):
+      faces = face_recognition.face_locations(frame)
+      try:
+        top,right,bottom,left = faces[0]
+        frame = frame[top:bottom, left:right, :]
+      except:
+        pass
+      frames.append(self.transform(frame))
+      if(len(frames) == self.count):
+        break
+    frames = torch.stack(frames)
+    frames = frames[:self.count]
+    return frames.unsqueeze(0)
 
-    def frame_extract(self, path):
-        vidObj = cv2.VideoCapture(path)
-        success = 1
-        while success:
-            success, image = vidObj.read()
-            if success:
-                yield image
+  # To extract number of frames
+  def frame_extract(self, path):
+    vidObj = cv2.VideoCapture(path)
+    success = 1
+    while success:
+      success, image = vidObj.read()
+      if success:
+        yield image
+
 
 def detectFakeVideo(videoPath):
     im_size = 112
@@ -127,30 +172,35 @@ def detectFakeVideo(videoPath):
     std = [0.229, 0.224, 0.225]
 
     train_transforms = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((im_size, im_size)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std)])
-    path_to_videos = [videoPath]
+                                        transforms.ToPILImage(),
+                                        transforms.Resize((im_size,im_size)),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean,std)])
+    path_to_videos= [videoPath]
 
-    video_dataset = validation_dataset(path_to_videos, sequence_length=20, transform=train_transforms)
+    video_dataset = validation_dataset(path_to_videos,sequence_length = 20,transform = train_transforms)
+    # use this command for gpu
+    # model = Model(2).cuda()
     model = Model(2)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    path_to_model = 'model/df_model.pt'
+    model.load_state_dict(torch.load(path_to_model, map_location=torch.device('cpu')))
     model.eval()
-    for i in range(0, len(path_to_videos)):
+    for i in range(0,len(path_to_videos)):
         print(path_to_videos[i])
-        prediction = predict(model, video_dataset[i], './')
+        prediction = predict(model,video_dataset[i],'./')
         if prediction[0] == 1:
             print("REAL")
         else:
             print("FAKE")
     return prediction
 
+
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
-    if request.method == 'GET':
-        return render_template('index.html')
-    return render_template('index.html')
+  if request.method == 'GET':
+	  return render_template('index.html')
+  return render_template('index.html')
+
 
 @app.route('/Detect', methods=['POST', 'GET'])
 def DetectPage():
@@ -165,14 +215,14 @@ def DetectPage():
         prediction = detectFakeVideo(video_path)
         print(prediction)
         if prediction[0] == 0:
-            output = "FAKE"
+              output = "FAKE"
         else:
-            output = "REAL"
+              output = "REAL"
         confidence = prediction[1]
         data = {'output': output, 'confidence': confidence}
         data = json.dumps(data)
-        os.remove(video_path)
+        os.remove(video_path);
         return render_template('index.html', data=data)
+        
 
-if __name__ == "__main__":
-    app.run(port=3000)
+app.run(port=3000);
